32ae9526b4475b4c5e4a1ffde7311b59eaae4ab0
Goran Cvijanovic <goran.rs.bg@gmail.com>
2022-05-23 19:45:26 +0200

    IOException logged, on error show label with message.

--- src/main/java/xyz/app/nlf/utils/ViewManager.java
+++ src/main/java/xyz/app/nlf/utils/ViewManager.java
@@ -1,29 +1,46 @@
-
 package xyz.app.nlf.utils;
 
 import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Lap
  */
 public class ViewManager {
-    
-   private static final ViewManager INSTANCE = new ViewManager();
-   
-   private ViewManager() {
-   
-   }
-   
-   public static ViewManager get() {
-       return INSTANCE;
-   }
-   
-   public Parent loadPrimaryFXML() throws IOException {
-        String fxmlFile = String.format("/fxml/%s.fxml", Views.PRIMARY.fileName());
-        FXMLLoader fxmlLoader = new FXMLLoader(ViewManager.class.getResource(fxmlFile));
-        return fxmlLoader.load();
-    }  
+
+    private static final ViewManager INSTANCE = new ViewManager();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ViewManager.class);
+
+    private BorderPane mainPane;
+
+    private ViewManager() {
+    }
+
+    public static ViewManager get() {
+        return INSTANCE;
+    }
+
+    public BorderPane loadPrimary() {
+        mainPane = (BorderPane) loadFXML(Views.PRIMARY);
+        return mainPane;
+    }
+
+    public Parent loadFXML(Views view) {
+        Parent parent = new Label("Error loading view.");
+        try {
+            String fxmlFile = String.format("/fxml/%s.fxml", view.fileName());
+            FXMLLoader fxmlLoader = new FXMLLoader(ViewManager.class.getResource(fxmlFile));
+            parent = fxmlLoader.load();
+        } catch (IOException ex) {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+        return parent;
+    }
 }
