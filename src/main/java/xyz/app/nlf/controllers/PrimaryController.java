abcb652111a1536f48aa529d87a32e8a031fa686
Goran Cvijanovic <goran.rs.bg@gmail.com>
2022-05-24 10:43:57 +0200

    book list view added

--- src/main/java/xyz/app/nlf/controllers/PrimaryController.java
+++ src/main/java/xyz/app/nlf/controllers/PrimaryController.java
@@ -1,9 +1,15 @@
 package xyz.app.nlf.controllers;
 
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.BorderPane;
+import xyz.app.nlf.jpa.dao.BooksDAO;
+import xyz.app.nlf.jpa.entity.Book;
+import xyz.app.nlf.utils.BookCellFactory;
 import xyz.app.nlf.utils.SharedData;
 import xyz.app.nlf.utils.ViewManager;
 import xyz.app.nlf.utils.Views;
@@ -14,9 +20,13 @@ public class PrimaryController {
     private Label messageLabel;
     @FXML
     private BorderPane borderPane;
+    @FXML
+    private ListView<Book> booksListView;
     
     public void initialize() {
         SharedData.get().setPrimaryController(this);
+        booksListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        booksListView.setCellFactory(new BookCellFactory());
     }
 
     @FXML
@@ -37,6 +47,12 @@ public class PrimaryController {
     @FXML
     private void onLoans(ActionEvent event) {
         changeCenterNode(Views.LOAN);
+    }
+    
+    @FXML
+    private void onRefreshBooks(ActionEvent event) {
+        List<Book> books = BooksDAO.get().readAll();
+        booksListView.getItems().addAll(books);
     }
     
     private void changeCenterNode(Views view) {
