3460dc2bd0fca2d2c035a346c208ffa0572c9d84
Goran Cvijanovic <goran.rs.bg@gmail.com>
2022-05-24 12:20:28 +0200

    construted same as book should work

--- src/main/java/xyz/app/nlf/controllers/StudentController.java
+++ src/main/java/xyz/app/nlf/controllers/StudentController.java
@@ -1,8 +1,12 @@
 
 package xyz.app.nlf.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import xyz.app.nlf.jpa.dao.BooksDAO;
+import xyz.app.nlf.jpa.dao.StudentsDAO;
 import xyz.app.nlf.jpa.entity.Book;
 import xyz.app.nlf.jpa.entity.Student;
 import xyz.app.nlf.utils.Settable;
@@ -16,6 +20,12 @@ public class StudentController implements Settable {
 
     @FXML
     private Label titleLabel;
+    @FXML
+    private Label idLabel;
+    @FXML
+    private TextField nameTextField;
+    
+    private Student loadedStudent;
     
     public void initialize() {
         SharedData.get().setSettableController(this);
@@ -29,7 +39,53 @@ public class StudentController implements Settable {
 
     @Override
     public void setStudent(Student student) {
+        populateStudent(student);
+    }
+
+    @FXML
+    private void onSave(ActionEvent event) {
+        if(loadedStudent != null && collectBook()) {
+            if(loadedStudent.getId() == 0) {
+                StudentsDAO.get().save(loadedStudent);
+            } else {
+                StudentsDAO.get().update(loadedStudent);
+            }
+        }
+    }
+
+    @FXML
+    private void onDelete(ActionEvent event) {
+        if(loadedStudent != null || loadedStudent.getId() > 0) {
+            StudentsDAO.get().delete(loadedStudent);
+            populateStudent(new Student());
+        }
+    }
+
+    @FXML
+    private void onNewItem(ActionEvent event) {
+        populateStudent(new Student());
+    }
+    
         
+    /**
+     * Init fields with the student data.
+     * 
+     * @param student 
+     */
+    private void populateStudent(Student student) {
+        idLabel.setText(student.getId() + "");
+        nameTextField.setText(student.getName());
+        loadedStudent = student;
+    }
+    
+    /**
+     * Collect data from fields and set in the student.
+     * 
+     * @return True if data are valid.
+     */
+    private boolean collectBook() {
+        loadedStudent.setName(nameTextField.getText());
+        return true;
     }
     
 }
